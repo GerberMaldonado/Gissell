369b433941f6b6666d0de4d34a26b1fa3583034b
Gerber Maldonado <g.maldonadof25@gmail.com>
2019-10-18 18:21:29 -0600

    Base de datos completa

--- Gissell/Apps/Admin/admin.py
+++ Gissell/Apps/Admin/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Persona
+from .models import Persona, TipoPersonaPersona, TipoPersona, TipoTerapia, Terapia 
+from .models import Curso, AsignacionCurso, Unidad, Nota, TipoDonacion, Donacion
 # Register your models here.
 
 admin.site.register(Persona)
+admin.site.register(TipoPersona)
+admin.site.register(TipoPersonaPersona)
+admin.site.register(TipoTerapia)
+admin.site.register(Terapia)
+admin.site.register(Curso)
+admin.site.register(AsignacionCurso)
+admin.site.register(Unidad)
+admin.site.register(Nota)
+admin.site.register(TipoDonacion)
+admin.site.register(Donacion)
--- Gissell/Apps/Admin/models.py
+++ Gissell/Apps/Admin/models.py
@@ -12,3 +12,43 @@ class Persona(models.Model):
 
     def __str__(self):
         return '%s %s '% (self.nombre, self.apellido)
+
+class TipoPersona(models.Model):
+    tipo_persona = models.CharField(max_length=50)
+
+class TipoPersonaPersona(models.Model):
+    persona_id = models.ForeignKey(Persona, on_delete=models.CASCADE)
+    tipo_persona_id = models.ForeignKey(TipoPersona, on_delete=models.CASCADE)
+
+class TipoTerapia(models.Model):
+    tipo_terapia = models.CharField(max_length=50)
+    sesiones = models.CharField(max_length=50)
+    avances = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=200)
+
+class Terapia(models.Model):
+    tipo_terapia_id = models.ForeignKey(TipoTerapia, on_delete=models.CASCADE)
+    tipo_persona_persona_id = models.ForeignKey(TipoPersonaPersona, on_delete=models.CASCADE)
+
+class Curso(models.Model):
+    curso = models.CharField(max_length=50)
+
+class AsignacionCurso(models.Model):
+    curso_id = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    tipo_persona_persona_id = models.ForeignKey(TipoPersonaPersona, on_delete=models.CASCADE)
+
+class Unidad(models.Model):
+    unidad = models.CharField(max_length=50)
+
+class Nota(models.Model):
+    nota = models.CharField(max_length=50)
+    unidad_id = models.ForeignKey(Unidad, on_delete=models.CASCADE)
+    tipo_persona_persona_id = models.ForeignKey(TipoPersonaPersona, on_delete=models.CASCADE)
+
+class TipoDonacion(models.Model):
+    tipo_donacion = models.CharField(max_length=50)
+
+class Donacion(models.Model):
+    monto = models.CharField(max_length=50)
+    tipo_donacion_id = models.ForeignKey(TipoDonacion, on_delete=models.CASCADE)
+    tipo_persona_persona_id = models.ForeignKey(TipoPersonaPersona, on_delete=models.CASCADE)
